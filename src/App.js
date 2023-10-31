1f10bcc788cf5a54d340e78ff83661c1bd58df36
Kestrel86 <valdezandrew20@gmail.com>
2023-10-31 10:31:21 -0700

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-3xl font-bold underline">Hello world!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

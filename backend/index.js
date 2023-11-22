ba1857b3df622011e9ea5016110789773d692ed1
Kestrel86 <valdezandrew20@gmail.com>
2023-11-21 18:01:17 -0800

    created backend folder

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = require("./routes/router");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use("/", router);
+
+const port = 4000; //modifiable on localhost:____ port
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//Video at 13 minutes
+
+//REact backend video by Codr kai
--- src/App.js
+++ src/App.js
@@ -15,12 +15,22 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// please dont forget to use the .map js function for listing your parent
+// and child components. it will map from an array the total number of
+// list object throught the array. use a protected dashboard as your main
+// page and use components to load on top. use the navigate() function
+// that is inside of the react router to get the response from the post
+// request upon the loging to pass the varibales from your login page/
+// omponent to the dashboard page. what you want to passs in is an id
+// anumber and the jwt token. this will allow you to pass the id number
+// back to the serve and simply run the token verify on the dahboard
+// page. then you can pass the id, once on the dahboard to the omponents
+// to render the data that is asociated with that id number.

4a4bc1692364f1915dec46f689f661e00d917267
Kenneth Hung <kennethhung@cpp.edu>
2023-11-30 13:01:18 -0800

    added support form, edited support schema

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -28,6 +28,30 @@ router.post("/contact/:a", async (req, res) => {
   res.end();
 });
 
+router.post("/support/:a", async (req, res) => {
+  const { name, email, donation, message } = req.body;
+  const action = req.params.a;
+
+  switch (action) {
+    case "send":
+      const supportData = { email: email, name: name, message: message, donation: donation };
+      const newSupport = new schemas.Support(supportData);
+      const saveSupport = await newSupport.save();
+      if (saveSupport) {
+        res.send("Message Sent. Thank you");
+      } else {
+        res.send("Failed to send message.");
+      }
+
+      break;
+
+    default:
+      res.send("Invalid request");
+      break;
+  }
+  res.end();
+});
+
 router.get("/users", async (req, res) => {
   const users = schemas.Users;
 
@@ -39,95 +63,23 @@ router.get("/users", async (req, res) => {
   const userData = [
     {
       id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: {
-          lat: "-37.3159",
-          lng: "81.1496",
-        },
-      },
-      phone: "1-770-736-8031 x56442",
-      website: "hildegard.org",
-      company: {
-        name: "Romaguera-Crona",
-        catchPhrase: "Multi-layered client-server neural-net",
-        bs: "harness real-time e-markets",
-      },
+      donation: "$0.00"
     },
     {
       id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        suite: "Suite 879",
-        city: "Wisokyburgh",
-        zipcode: "90566-7771",
-        geo: {
-          lat: "-43.9509",
-          lng: "-34.4618",
-        },
-      },
-      phone: "010-692-6593 x09125",
-      website: "anastasia.net",
-      company: {
-        name: "Deckow-Crist",
-        catchPhrase: "Proactive didactic contingency",
-        bs: "synergize scalable supply-chains",
-      },
+      donation: "$1.00"
     },
     {
       id: 3,
-      name: "Clementine Bauch",
-      username: "Samantha",
-      email: "Nathan@yesenia.net",
-      address: {
-        street: "Douglas Extension",
-        suite: "Suite 847",
-        city: "McKenziehaven",
-        zipcode: "59590-4157",
-        geo: {
-          lat: "-68.6102",
-          lng: "-47.0653",
-        },
-      },
-      phone: "1-463-123-4447",
-      website: "ramiro.info",
-      company: {
-        name: "Romaguera-Jacobson",
-        catchPhrase: "Face to face bifurcated interface",
-        bs: "e-enable strategic applications",
-      },
+      donation: "$5.00"
     },
     {
       id: 4,
-      name: "Patricia Lebsack",
-      username: "Karianne",
-      email: "Julianne.OConner@kory.org",
-      address: {
-        street: "Hoeger Mall",
-        suite: "Apt. 692",
-        city: "South Elvis",
-        zipcode: "53919-4257",
-        geo: {
-          lat: "29.4572",
-          lng: "-164.2990",
-        },
-      },
-      phone: "493-170-9623 x156",
-      website: "kale.biz",
-      company: {
-        name: "Robel-Corkery",
-        catchPhrase: "Multi-tiered zero tolerance productivity",
-        bs: "transition cutting-edge web services",
-      },
+      donation: "$10.00"
+    },
+    {
+      id: 5,
+      donation: "$69.69"
     },
   ];
 

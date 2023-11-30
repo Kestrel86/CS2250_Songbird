ac10655375a446c317521f9b09730ea7603f11c2
Kestrel86 <90483954+Kestrel86@users.noreply.github.com>
2023-11-30 13:05:38 -0800

    changes to fetch

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,22 +1,42 @@
 import "../assets/css/box.css";
 import { useState, useEffect, useCallback } from "react";
 import Box from "./Box";
+import axios from "axios";
 
 //file where automatic generation occurs
 
 export default function Boxes(props) {
+  const [selectData, setSelectData] = useState([]);
+  const [selectValue, setSelectValue] = useState("");
+  const [boxes, setBoxes] = useState([]);
+
   const { email, name, msg, donation } = props;
 
-  const [boxes, setBoxes] = useState([]);
+  const axiosFetchData = async (processing) => {
+    await axios
+      .get("http://localhost:4000/users")
+      .then((res) => {
+        if (processing) {
+          setSelectData(res.data);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <div className="container">
       <h1>Sponsors:</h1>
       <div className="items">
-        {/* {boxes?.map((email, name, msg, donation) => {
-          return <Box />;
-        })} */}
-        <Box />
+        {boxes?.map((email, name, msg, donation) => {
+          return (
+            <Box
+              email={props.email}
+              name={props.name}
+              msg={props.msg}
+              donation={props.donation}
+            />
+          );
+        })}
         <Box />
       </div>
     </div>
